216abe8792e9dfe6cfe84df455a4df79064372fd
Yreck-RU <74424990+Yreck-RU@users.noreply.github.com>
2023-04-24 15:12:37 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -48,3 +48,70 @@ document.addEventListener( 'click', (e) => {
 })
 //======================================================
 
+
+const selectSingles = document.querySelectorAll('.select');
+
+if (selectSingles) {
+	for (let i = 0; i < selectSingles.length; i++) {
+		const selectSingle = selectSingles[i];
+		const selectSingle_title = selectSingle.querySelector('.select-title');
+		const selectSingle_labels = selectSingle.querySelectorAll('.select-content__label');
+
+		// Toggle menu
+		selectSingle_title.addEventListener('click', () => {
+			if ('active' === selectSingle.getAttribute('data-state')) {
+				//selectSingle.setAttribute('data-state', '');
+			} else {
+				selectSingle.setAttribute('data-state', 'active');
+				if (selectSingle_title.classList.contains('__select__title-countries')) {
+
+				}
+			}
+		});
+
+		document.addEventListener( 'click', (e) => {
+			let withinBoundaries = e.composedPath().includes(selectSingle_title);
+			let withinBoundaries2 = e.composedPath().includes(selectSingle_labels);
+			if ( ! withinBoundaries && ! withinBoundaries2) {
+				selectSingle.setAttribute('data-state', '');
+			}
+			
+		})
+
+		// Close when click to option
+		for (let i = 0; i < selectSingle_labels.length; i++) {
+			let selectSingle_label = selectSingle_labels[i];
+			selectSingle_labels[i].addEventListener('click', (evt) => {
+				if (selectSingle_title.querySelector('input')) {
+					selectSingle_title.querySelector('input').value = evt.target.textContent;
+					//console.log(evt.target.textContent);
+				}
+				selectSingle.setAttribute('data-state', '');
+			});
+		}
+	}
+}
+
+/*===========================================*/
+
+const panels = document.querySelectorAll('.editing-content-sample-panel');
+
+if (panels) {
+	for (let i = 0; i < panels.length; i++) {
+		let panel = panels[i];
+		let panelButton = panel.querySelector('.editing-content-sample-panel__button');
+		let panelWrapper = panel.querySelector('.editing-content-sample-panel__wrapper');
+
+		panelButton.addEventListener("click", function (e) {
+			panel.classList.toggle('_active');
+		});
+		document.addEventListener( 'click', (e) => {
+			let withinBoundaries = e.composedPath().includes(panelButton);
+			let withinBoundaries2 = e.composedPath().includes(panelWrapper);
+			if ( ! withinBoundaries && ! withinBoundaries2) {
+				panel.classList.remove('_active');
+			}
+			
+		})
+	}
+}
